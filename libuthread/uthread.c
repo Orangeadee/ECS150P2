f668bef467f1b074a71817b1556c64f71544c72c
Huilin Zhang <orangeade93626@gmail.com>
2022-02-02 21:45:08 -0800

    Updated uthread_create() and uthread_self()

--- libuthread/uthread.c
+++ libuthread/uthread.c
@@ -1,6 +1,7 @@
 #include <assert.h>
 #include <limits.h>
 #include <signal.h>
+#include <stdbool.h>
 #include <stddef.h>
 #include <stdint.h>
 #include <stdio.h>
@@ -12,6 +13,16 @@
 #include "queue.h"
 
 /* TODO */
+
+/* 
+ *								*
+ * Initialization:						*
+ * - Following the prompt, create a type of data structure	*
+ *   called @TCB to store all informations that a thread must	*
+ *   contain.							*
+ * - The state @s stores the current status of a thread		*
+ *								*
+ */
 enum state {ready,running,blocked,zombie,exited};
 
 struct TCB {
@@ -25,8 +36,20 @@ struct TCB {
 	queue_t blocked_q;
 };
 
+/* Record the current running thread */
 struct TCB *curr;
 
+/*
+ *								*
+ * uthread_start:						*
+ * - Allocating spaces for a new thread and initialize all the	*
+ *   components inside a TCB.					*
+ * - If preempt is true, enable the preempt function and record	*
+ *   @preemptive as true, otherwise disable the preempt		*
+ * - Since we just started a new thread, its information will	*
+ *   be sotred in @curr as current running thread		*
+ *								*
+ */
 int uthread_start(int preempt)
 {
 
@@ -68,6 +91,21 @@ int uthread_stop(void)
 int uthread_create(uthread_func_t func)
 {
 	/* TODO */
+	struct TCB *create_thread = (struct TCB*)malloc(sizeof(struct TCB));
+	if(create_thread == NULL || curr->tid >= USHRT_MAX)
+	{
+		return -1;
+	}
+	curr->tid++;
+	create_thread->tid = curr->tid;
+	create_thread->stack = uthread_ctx_alloc_stack();
+	create_thread->s = ready;
+	int status = uthread_ctx_init(&create_thread->save_ctx,create_thread->stack,func);
+	if(status == 0)
+	{
+		assert(queue_enqueue(curr->ready_q,create_thread));
+		return create_thread->tid;
+	}
 	return -1;
 }
 
@@ -76,10 +114,16 @@ void uthread_yield(void)
 	/* TODO */
 }
 
+/*
+ * 						*
+ * uthread_self:				*
+ * - return the tid of current running thread 	*
+ *						*
+ */
 uthread_t uthread_self(void)
 {
 	/* TODO */
-	return -1;
+	return curr->tid;
 }
 
 void uthread_exit(int retval)
