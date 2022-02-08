f89d1ba886cb574a83da98f46be546b98e635806
SS041 <63644018+SS041@users.noreply.github.com>
2022-02-08 13:04:18 -0800

    Update preempt.c
    Add preempt

--- libuthread/preempt.c
+++ libuthread/preempt.c
@@ -18,20 +18,43 @@
 void preempt_start(void)
 {
 	/* TODO */
+	struct itimerval new;
+  	new.it_interval.tv_usec = 10;
+  	new.it_interval.tv_sec = 0;
+  	new.it_value.tv_usec = 10;
+  	new.it_value.tv_sec = 0;
+
+	void termination_handler (int signum);
+	signal (SIGVTALRM, termination_handler);
+	setitimer (ITIMER_VIRTUAL, &new, NULL);
 }
 
 void preempt_stop(void)
 {
 	/* TODO */
+	struct itimerval new;  
+    new.it_interval.tv_usec = 0;
+  	new.it_interval.tv_sec = 0;
+  	new.it_value.tv_usec = 0;
+  	new.it_value.tv_sec = 0;
+    setitimer(ITIMER_VIRTUAL, &new, NULL);  
 }
 
 void preempt_enable(void)
 {
 	/* TODO */
+	sigset_t set;
+	sigemptyset(&set);
+	sigaddset(&set, SIGVTALRM);
+	sigprocmask (SIG_BLOCK, &set, NULL);
 }
 
 void preempt_disable(void)
 {
 	/* TODO */
+	sigset_t set;
+	sigemptyset(&set);
+	sigaddset(&set, SIGVTALRM);
+	sigprocmask (SIG_BLOCK, &set, NULL);
 }
 
